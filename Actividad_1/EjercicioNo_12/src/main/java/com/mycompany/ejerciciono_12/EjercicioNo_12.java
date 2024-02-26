732ae07d1616f6228182a3e5ea34c51ef37d2899
Juanes2001 <jrodriguezoc@unal.edu.co>
2024-02-25 21:38:49 -0500

    Solucion Ejercicio No_5

---
+++ Actividad_1/EjercicioNo_12/src/main/java/com/mycompany/ejerciciono_12/EjercicioNo_12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciono_12;
+
+/**
+ *
+ * @author juane
+ */
+public class EjercicioNo_12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Actividad_1/EjercicioNo_5/src/main/java/com/mycompany/ejerciciono_5/EjercicioNo_5.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciono_5;
+
+/**
+ *
+ * @author juane
+ */
+public class EjercicioNo_5 {
+
+    public static void main(String[] args) {
+        //Creacion de variables a usar
+        double SUMA,x,y;
+        
+        //Asignacion de valores 
+        SUMA = 0;
+        x = 20;
+        SUMA = SUMA + x;
+        y = 40;
+        x= x + Math.pow(y, 2);
+        SUMA = SUMA + x/y;
+        
+        //Escritura en consola del resultado de suma:
+        
+        System.out.println("EL VALOR DE LA SUMA ES: " + SUMA);
+        
+        
+        
+        
+        
+    }
+}
