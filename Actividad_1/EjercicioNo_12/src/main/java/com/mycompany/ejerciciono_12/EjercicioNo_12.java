213c405b814379f7e00c088555aa0cfc5341fe69
Juanes2001 <jrodriguezoc@unal.edu.co>
2024-02-25 21:51:23 -0500

    Solucion ejercicio No_12

--- Actividad_1/EjercicioNo_12/src/main/java/com/mycompany/ejerciciono_12/EjercicioNo_12.java
+++ Actividad_1/EjercicioNo_12/src/main/java/com/mycompany/ejerciciono_12/EjercicioNo_12.java
@@ -11,6 +11,27 @@ package com.mycompany.ejerciciono_12;
 public class EjercicioNo_12 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Se declaran las variables, el Salario bruto, el neto y la retencion
+        // en fuente.
+        double salB, salN, ret;
+        
+        //Se calcula el salario bruto como la rata de salario por hora multiplicado
+        // el numero total de horas, el resultado es el salario en pesos 
+        salB = 48*5000;
+        
+        // La retencion es un 12.5 por ciento del salario bruto, este se calcula
+        // multiplicando el salario bruto por el porcentaje de retencion:
+        
+        ret = salB * 12.5/100;
+        
+        // El salario neto se calcula restando la retencion en la fuente al
+        //salario bruto
+        
+        salN = salB -ret;
+        
+        // Se muestran los datos pedidos
+        System.out.println(" El salario bruto del trabajador es : " + salB);
+        System.out.println(" El salario neto del trabajador es : " + salN);
+        System.out.println(" La retencion en la fuente es : " + ret);
     }
 }
