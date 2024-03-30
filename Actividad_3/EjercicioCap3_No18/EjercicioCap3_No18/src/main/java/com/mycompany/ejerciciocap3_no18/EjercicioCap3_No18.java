e2026c6f5caa0b1af5f89f96d23b1e357c5c8eac
Juanes2001 <jrodriguezoc@unal.edu.co>
2024-03-30 13:44:06 -0500

    Solucion ejercicioo cap3 Numero 18

---
+++ Actividad_3/EjercicioCap3_No18/EjercicioCap3_No18/src/main/java/com/mycompany/ejerciciocap3_no18/EjercicioCap3_No18.java
@@ -0,0 +1,17 @@
+   /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciocap3_no18;
+
+public class EjercicioCap3_No18 {
+
+    public static void main(String[] args) {
+        
+        EjercicioC3N18 display = new EjercicioC3N18();
+        
+        display.setVisible(true);
+        display.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ Actividad_3/EjercicioCap3_No19/src/main/java/com/mycompany/ejerciciocap3_no19/EjercicioCap3_No19.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciocap3_no19;
+
+/**
+ *
+ * @author juane
+ */
+public class EjercicioCap3_No19 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
