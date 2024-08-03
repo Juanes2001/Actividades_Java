7eeaf04d5896606d15f18a6cd834e173bc18b481
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2024-08-02 22:29:32 -0500

    Adelanto para crear UML desde NetBeans

--- Actividad_3/EjercicioCap3_No18/EjercicioCap3_No18/src/main/java/com/mycompany/ejerciciocap3_no18/Trabajo.java
+++ Actividad_3/EjercicioCap3_No18/EjercicioCap3_No18/src/main/java/com/mycompany/ejerciciocap3_no18/Trabajo.java
@@ -1,9 +1,6 @@
 package com.mycompany.ejerciciocap3_no18;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *
