4b386049ce6ee34bd91c190420b891c6b8a3329f
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2024-08-03 20:46:13 -0500

    Actualizacion Netbeans

---
+++ Actividad_3/ejericicioCap3_No18/EjercicioCap3_no18/src/main/java/com/mycompany/ejerciciocap3_no18/Code.java
@@ -0,0 +1,20 @@
+package com.mycompany.ejerciciocap3_no18;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author juane
+ */
+public class Code {
+    
+    String codigo;
+    // Con esta funcion definimos el valor de codigo 
+    public void codeDefine (String code){
+        codigo = code;
+    }
+    
+}
---
+++ Actividad_3/ejericicioCap3_No18/EjercicioCap3_no18/src/main/java/com/mycompany/ejerciciocap3_no18/EjercicioCap3_no18.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejerciciocap3_no18;
+
+/**
+ *
+ * @author juane
+ */
+public class EjercicioCap3_no18 {
+
+     public static void main(String[] args) {
+        
+        EjercicioC3N18 display = new EjercicioC3N18();
+        
+        display.setVisible(true);
+        display.setLocationRelativeTo(null);
+    }
+}
